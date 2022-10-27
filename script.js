12ac8805de06aa96addb1efbbf230bc0dca7b7b9
alexeerma <aleksander.eerma@hotmail.com>
2022-10-27 23:17:35 +0300

    Etch a Sketch done with extra css changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container')
 const btnBlack = document.createElement('button')
 const btnRgb = document.createElement('button')
-const btnGray = document.createElement('button')
+const btnWhite = document.createElement('button')
 const btnSize = document.createElement('button')
 const section = document.querySelector('.section');
 const buttonsContainer = document.querySelector('.buttons');
@@ -24,20 +24,20 @@ function creatDivs(col , rows) {
 }
 creatDivs(16,16)
 
-function grayColor() { 
+function whiteColor() { 
     
     const boxs = container.querySelectorAll('.box')
-    btnGray.textContent = "GRAY"
-    btnGray.addEventListener('click', () => {
-        boxs.forEach(box => box.addEventListener('mouseover', ()=> {
-            let RNum = Math.floor(Math.random() * 256);
-            let GrayScale = `rgb(${RNum},${RNum},${RNum})`
-            box.style.background = GrayScale;
+    btnWhite.textContent = "ERASER"
+    btnWhite.addEventListener('click', () => {
+        boxs.forEach(box => box.addEventListener('mouseover', function() {
+            this.style.background = 'rgb(240,255,240)'
         }))
     })
-    buttonsContainer.appendChild(btnGray).classList.add('btn')
+    buttonsContainer.appendChild(btnWhite).classList.add('btn')
 }
-grayColor()
+whiteColor()
+
+
 
 
 function rgbColors() {
@@ -86,13 +86,13 @@ function reSize() {
         if(user === null || user < 1){
             reSet();
             creatDivs(16,16);
-            grayColor();
+            whiteColor();
             rgbColors();
             blackColor();
         } else { 
             reSet();
             creatDivs(user,user);
-            grayColor();
+            whiteColor();
             rgbColors();
             blackColor();
         }
